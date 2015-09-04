c0b00a46e01c3de9d321b3dace6a1bbb01936ea0
joh12041 <joh12041@umn.edu>
2015-09-04 09:58:00 -0500

    Try/except around mapping of losses because this caused a null geometry error and i'm not sure if i fixed it but it's not important enough to crash the code

--- experiments/2016_AlgorithmicImpact/code/model_test.py
+++ experiments/2016_AlgorithmicImpact/code/model_test.py
@@ -699,9 +699,12 @@ class Test(object):
                                         'contour', 'pra50', 'pra90', 'pra95',
                                         'covt95', 'covt90', 'covt50'])
         if losses:
-            self.map_tweets(losses, "{0}/test_failed_tweets_{1}.csv".format(outputdir, self.i),
-                            cur,
-                            properties=[])
+            try:
+                self.map_tweets(losses, "{0}/test_failed_tweets_{1}.csv".format(outputdir, self.i),
+                                cur,
+                                properties=[])
+            except:
+                print("Failed to map out {0} losses.".format(len(losses)))
 
 
     def where(self, phase, column):
