32eac6dc774c8c30db3ca5a04fe80bee4b845548
Isaac J <joh12041@umn.edu>
2015-10-21 15:31:44 -0500

    update shell script ot run just the more urban model

--- experiments/2016_AlgorithmicImpact/tests/final_tests/urban_123.imm.sh
+++ experiments/2016_AlgorithmicImpact/tests/final_tests/urban_123.imm.sh
@@ -1,50 +1,8 @@
 #!/bin/bash
 
-# Progressively more skewed urban models
+# Progressively more skewed urban model
 
 . $(dirname $0)/parseargs.sh
-
-model-test \
-    --min-instances 3 \
-    --model geo.gmm.Token \
-    --model-parms weight_f:wt_inv_error_sae \
-    --test-tweet-limit 60000 \
-    --train-tweet-limit 30000 \
-    --start $START \
-    --end $END \
-    --training P2D \
-    --testing P3D \
-    --gap P1D \
-    --stride P2D \
-    --cores $CORE_CT \
-    --limit 5 \
-    --skip-small-tests 0 \
-    --verbose \
-    --ses urban \
-    --how_filter urban3 \
-    --filter_testing 0 \
-    $GEODB $JOBDIR/tr_urbanthree30k_te_rand60k
-
-model-test \
-    --min-instances 3 \
-    --model geo.gmm.Token \
-    --model-parms weight_f:wt_inv_error_sae \
-    --test-tweet-limit 60000 \
-    --train-tweet-limit 30000 \
-    --start $START \
-    --end $END \
-    --training P2D \
-    --testing P3D \
-    --gap P1D \
-    --stride P2D \
-    --cores $CORE_CT \
-    --limit 5 \
-    --skip-small-tests 0 \
-    --verbose \
-    --ses urban \
-    --how_filter urban2 \
-    --filter_testing 0 \
-    $GEODB $JOBDIR/tr_urbantwo30k_te_rand60k
 
 model-test \
     --min-instances 3 \
