efff390e503a29b60f3b5aa795da0a8c3e296c04
joh12041 <joh12041@umn.edu>
2015-10-29 14:13:23 -0500

    four and five didn't run for some reason

--- experiments/2016_AlgorithmicImpact/tests/final_tests/rural_12345.imm.sh
+++ experiments/2016_AlgorithmicImpact/tests/final_tests/rural_12345.imm.sh
@@ -4,68 +4,68 @@
 
 . $(dirname $0)/parseargs.sh
 
-model-test \
-    --min-instances 3 \
-    --model geo.gmm.Token \
-    --model-parms weight_f:wt_inv_error_sae \
-    --test-tweet-limit 60000 \
-    --train-tweet-limit 30000 \
-    --start $START \
-    --end $END \
-    --training P4D \
-    --testing P6D \
-    --gap P1D \
-    --stride P2D \
-    --cores $CORE_CT \
-    --limit 5 \
-    --skip-small-tests 0 \
-    --verbose \
-    --ses urban \
-    --how_filter rural1 \
-    --filter_testing 0 \
-    $GEODB $JOBDIR/tr_ruralone30k_te_rand60k
+#model-test \
+#    --min-instances 3 \
+#    --model geo.gmm.Token \
+#    --model-parms weight_f:wt_inv_error_sae \
+#    --test-tweet-limit 60000 \
+#    --train-tweet-limit 30000 \
+#    --start $START \
+#    --end $END \
+#    --training P4D \
+#    --testing P6D \
+#    --gap P1D \
+#    --stride P2D \
+#    --cores $CORE_CT \
+#    --limit 5 \
+#    --skip-small-tests 0 \
+#    --verbose \
+#    --ses urban \
+#    --how_filter rural1 \
+#    --filter_testing 0 \
+#    $GEODB $JOBDIR/tr_ruralone30k_te_rand60k
 
-model-test \
-    --min-instances 3 \
-    --model geo.gmm.Token \
-    --model-parms weight_f:wt_inv_error_sae \
-    --test-tweet-limit 60000 \
-    --train-tweet-limit 30000 \
-    --start $START \
-    --end $END \
-    --training P4D \
-    --testing P6D \
-    --gap P1D \
-    --stride P2D \
-    --cores $CORE_CT \
-    --limit 5 \
-    --skip-small-tests 0 \
-    --verbose \
-    --ses urban \
-    --how_filter rural2 \
-    --filter_testing 0 \
-    $GEODB $JOBDIR/tr_ruraltwo30k_te_rand60k
+#model-test \
+#    --min-instances 3 \
+#    --model geo.gmm.Token \
+#    --model-parms weight_f:wt_inv_error_sae \
+#    --test-tweet-limit 60000 \
+#    --train-tweet-limit 30000 \
+#    --start $START \
+#    --end $END \
+#    --training P4D \
+#    --testing P6D \
+#    --gap P1D \
+#    --stride P2D \
+#    --cores $CORE_CT \
+#    --limit 5 \
+#    --skip-small-tests 0 \
+#    --verbose \
+#    --ses urban \
+#    --how_filter rural2 \
+#    --filter_testing 0 \
+#    $GEODB $JOBDIR/tr_ruraltwo30k_te_rand60k
 
-model-test \
-    --min-instances 3 \
-    --model geo.gmm.Token \
-    --model-parms weight_f:wt_inv_error_sae \
-    --test-tweet-limit 60000 \
-    --train-tweet-limit 30000 \
-    --start $START \
-    --end $END \
-    --training P4D \
-    --testing P6D \
-    --gap P1D \
-    --stride P2D \
-    --cores $CORE_CT \
-    --limit 5 \
-    --skip-small-tests 0 \
-    --verbose \
-    --ses urban \
-    --how_filter rural3 \
-    --filter_testing 0 \
-    $GEODB $JOBDIR/tr_ruralthree30k_te_rand60k
+#model-test \
+#    --min-instances 3 \
+#    --model geo.gmm.Token \
+#    --model-parms weight_f:wt_inv_error_sae \
+#    --test-tweet-limit 60000 \
+#    --train-tweet-limit 30000 \
+#    --start $START \
+#    --end $END \
+#    --training P4D \
+#    --testing P6D \
+#    --gap P1D \
+#    --stride P2D \
+#    --cores $CORE_CT \
+#    --limit 5 \
+#    --skip-small-tests 0 \
+#    --verbose \
+#    --ses urban \
+#    --how_filter rural3 \
+#    --filter_testing 0 \
+#    $GEODB $JOBDIR/tr_ruralthree30k_te_rand60k
 
 model-test \
     --min-instances 3 \
