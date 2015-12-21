f263644cfd5b76fbe80bad29e0c8c8796efdcd7e
Isaac J <joh12041@umn.edu>
2015-12-21 04:39:55 -0600

    actual boosting beyond 30k training

---
+++ experiments/2016_AlgorithmicImpact/tests/final_tests/boostall_final.imm.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+# Train only urban (1-2)
+
+. $(dirname $0)/parseargs.sh
+
+model-test \
+    --min-instances 3 \
+    --model geo.gmm.Token \
+    --model-parms weight_f:wt_inv_error_sae \
+    --test-tweet-limit 60000 \
+    --train-tweet-limit 30000 \
+    --start $START \
+    --end $END \
+    --training P2D \
+    --testing P3D \
+    --gap P1D \
+    --stride P2D \
+    --cores $CORE_CT \
+    --limit 2 \
+    --skip-small-tests 0 \
+    --verbose \
+    --ses urban \
+    --how_filter extra_r \
+    --filter_testing 0 \
+    $GEODB $JOBDIR/tr_boostrural30k_te_rand60k
+
+model-test \
+    --min-instances 3 \
+    --model geo.gmm.Token \
+    --model-parms weight_f:wt_inv_error_sae \
+    --test-tweet-limit 60000 \
+    --train-tweet-limit 30000 \
+    --start $START \
+    --end $END \
+    --training P2D \
+    --testing P3D \
+    --gap P1D \
+    --stride P2D \
+    --cores $CORE_CT \
+    --limit 2 \
+    --skip-small-tests 0 \
+    --verbose \
+    --ses urban \
+    --how_filter extra_a \
+    --filter_testing 0 \
+    $GEODB $JOBDIR/tr_boostall30k_te_rand60k
